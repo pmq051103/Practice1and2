0de8ae252a7f967d713166cc75d114341f3d8c4b
pmq051103 <144422686+pmq051103@users.noreply.github.com>
2025-02-11 21:33:41 +0700

    Done practice1

---
+++ practice1/main.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", loadTasks);
+const API_URL = "http://localhost:3000/tasks";
+
+async function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+    if (taskText === "") return;
+
+    let randomId = Math.floor(100 + Math.random() * 900);
+    let idString = randomId.toString();
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: idString, text: taskText, completed: false })
+    });
+
+    taskInput.value = "";
+    loadTasks();
+}
+
+async function loadTasks() {
+    let taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    let response = await fetch(API_URL);
+    let tasks = await response.json();
+
+    tasks.forEach((task) => {
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+        span.textContent = task.text;
+        span.className =task.completed ? 'completed' : '';
+        if (!task.completed) {
+            span.onclick = () => toggleTask(task.id);
+        } else {
+            span.style.cursor = "not-allowed"; 
+        }
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Xóa";
+        deleteBtn.id="btnDelete";
+        deleteBtn.onclick = () => deleteTask(task.id);
+
+        li.appendChild(span);
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    });
+}
+
+async function toggleTask(id) {
+    let response = await fetch(`${API_URL}/${id}`);
+    let task = await response.json();
+
+    if (!task.completed) {
+        await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ text: task.text, completed: !task.completed })
+        });
+        loadTasks();
+    }
+}
+
+async function deleteTask(id) {
+    await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    loadTasks();
+}
